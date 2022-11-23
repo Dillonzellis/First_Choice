8a453088b07fea1f4188cf0da388e23b9c1ef031
Dillon Ellis <dillonzellis@gmail.com>
2022-11-23 09:35:34 -0500

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,21 @@
-const  path = require("path");
+const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: 'development',
-  output:{
+  mode: "development",
+  output: {
     // filename:"dist",
-    path: path.join(__dirname, 'GBD_Theme/dist'),
+    path: path.join(__dirname, "GBD_Theme/dist"),
   },
   plugins: [new MiniCssExtractPlugin()],
   module: {
-    rules: [{
-      test: /\.(s[ac]|c)ss$/i,
-      use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-    }]
-  }
+    rules: [
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
 };
+
+s;
