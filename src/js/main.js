3bb68ae87d9abc97f585844b1b2c65283ab58171
Dillon Ellis <dellis@fcul.com>
2022-11-30 15:16:56 -0500

    nav stlying

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
 console.log('working');
+
+// when .mega-menu-open toggle .nav-row-col display flex
+
+
+const megaMenuToggle = document.querySelector(".mega-menu-toggle");
+const testing = document.querySelector("#mega-menu-wrap-primary-menu");
+
+const navRowCol = document.querySelector(".nav-row-col");
+
+let toggleFlex = () => {
+    navRowCol.style.display = "none";
+    console.log('clciked')
+}
+
+const checking = () => {
+    if (testing.classList.contains('.mega-menu-open')) {
+        console.log('eureka');
+    }
+    console.log('something');
+}
+
+megaMenuToggle.addEventListener('click', checking)
+
+// navRowCol.addEventListener('click', console.log('yeppers'))
