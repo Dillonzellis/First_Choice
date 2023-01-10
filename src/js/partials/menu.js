22c16f37aacab0a41600d507aba91e84f1119b5e
Dillon Ellis <dillonzellis@gmail.com>
2023-01-10 15:48:02 -0500

    update content carousel member link and combine js files

---
+++ src/js/partials/menu.js
@@ -0,0 +1,8 @@
+const megaMenuToggle = document.querySelector(".mega-menu-toggle");
+const testing = document.querySelector("#mega-menu-wrap-primary-menu");
+
+const navRowCol = document.querySelector(".nav-row-col");
+
+let toggleFlex = () => {
+    navRowCol.style.display = "none";
+}
